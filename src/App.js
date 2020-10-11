92be3596d1afa5d3966028138b49ca3c6c7c913a
andy <andyst81@users.noreply.github.com>
2020-10-11 16:41:26 +0700

    More mobile styling edits

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
       <header className="App-header">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.min.css" integrity="sha512-xiunq9hpKsIcz42zt0o2vCo34xV0j6Ny8hgEylN3XBglZDtTZ2nwnqF/Z/TTCc18sGdvCjbFInNd++6q3J0N6g==" crossOrigin="anonymous" />
       <br />
-      <br />
       <h1 style={{textAlign:"center"}}> Random Image Game </h1>
         <Icons />
       </header>
